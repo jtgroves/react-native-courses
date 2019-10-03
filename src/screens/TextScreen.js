783acd5f3e6f3c7ee02aca54647d27c229cd18d0
Joseph Groves <jgroves.nc@gmail.com>
2019-10-02 23:22:01 -0400

    completed task

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [password, setPassword] = useState('');
   return (
     <View>
-      <TextInput />
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={newValue => setPassword(newValue)}
+      />
+      {password.length <= 6 ? (
+        <Text>Password mush be longer than 5 characters</Text>
+      ) : null}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
